c40f1bae77a8bc89c4a5e6d84e6c6e819ac0978f
swb23 <sander.berning@gmail.com>
2016-08-25 21:48:02 +0200

    Update cam-test.py

--- cam-test.py
+++ cam-test.py
@@ -7,7 +7,7 @@ import time
 camera = picamera.PiCamera(resolution=(1024, 768), framerate=24)
 camera.annotate_background = picamera.Color('black')
 camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-filename=(dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+ '.h264')
+filename=(dt.datetime.now().strftime('%Y%m%d_%H-%M-%S')+ '.h264')
 print(filename)
 camera.start_recording(filename)
 start = dt.datetime.now()
