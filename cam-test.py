87c7a331120c41bf73e82f996d095532ef00c1bc
swb23 <sander.berning@gmail.com>
2016-08-25 21:57:54 +0200

    Update cam-test.py

--- cam-test.py
+++ cam-test.py
@@ -8,17 +8,17 @@ camera = picamera.PiCamera(resolution=(1024, 768), framerate=24)
 camera.annotate_background = picamera.Color('black')
 camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 filename=(dt.datetime.now().strftime('%Y%m%d_%H-%M-%S')+ '.h264')
-    try:
-        # Loop until users quits with CTRL-C
-        while True:
-            camera.start_recording(filename)
-            start = dt.datetime.now()
-            while (dt.datetime.now() - start).seconds < 10:
-                camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                camera.wait_recording(0.2)
-    except KeyboardInterrupt:
-        # Reset GPIO settings
-        camera.stop_recording()
+try:
+     # Loop until users quits with CTRL-C
+    while True:
+        camera.start_recording(filename)
+        start = dt.datetime.now()
+        while (dt.datetime.now() - start).seconds < 10:
+            camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            camera.wait_recording(0.2)
+except KeyboardInterrupt:
+    # Reset GPIO settings
+    camera.stop_recording()
     
       
 
