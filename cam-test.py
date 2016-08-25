27cb4aa1ae591c513722e25273fc6f307b872781
swb23 <sander.berning@gmail.com>
2016-08-25 22:00:14 +0200

    Update cam-test.py

--- cam-test.py
+++ cam-test.py
@@ -11,14 +11,15 @@ filename=(dt.datetime.now().strftime('%Y%m%d_%H-%M-%S')+ '.h264')
 try:
      # Loop until users quits with CTRL-C
     while True:
-        camera.start_recording(filename)
-        start = dt.datetime.now()
-        while (dt.datetime.now() - start).seconds < 10:
+          camera.start_recording(filename)
+          start = dt.datetime.now()
+          while (dt.datetime.now() - start).seconds < 10:
             camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             camera.wait_recording(0.2)
+          camera.stop_recording()
 except KeyboardInterrupt:
     # Reset GPIO settings
-    camera.stop_recording()
+    continue
     
       
 
