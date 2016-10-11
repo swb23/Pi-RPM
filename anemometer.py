3e239ddf0d82b439f3b105c830eda29870111e19
Sander Berning <sander.berning@gmail.com>
2016-10-11 17:14:07 +0200

    Zwei Sensoren

--- anemometer.py
+++ anemometer.py
@@ -30,8 +30,9 @@ global db
 # Teilt der GPIO Bibliotek mit die GPIO references zu nutzen
 GPIO.setmode(GPIO.BCM)
 
-# Setzt Pin 27 als Inut
+# Setzt Pin 27 und Pin 17 als Inut
 GPIO.setup(27 , GPIO.IN)
+GPIO.setup(17 , GPIO.IN)
 
 class timer(threading.Thread):
     def __init__(self):
@@ -42,61 +43,64 @@ class timer(threading.Thread):
     def run(self):
         while True:
             time.sleep(aufloesung)
-            rpm=mittelwert()
-            
+            turbinerpm=counter.mittelwert(zws1)
+            windrpm=counter.mittelwert(zws2)
             print(rpm)
-            speichern(rpm)
-            
-       
-    # Ermittelt aus den Messdaten des Sensors einen Durchschnittswert für die Dauer der Aufloesung     
-def mittelwert():
-    global zws
-    t1=dt.datetime.now()-dt.timedelta(0,aufloesungzm)
-    while True:
-        if len(zws)==0:
-            break   # Loescht alle Werte die aelter als die dauer der Aufloesung sind
-        elif t1>=zws[0]:
-            del zws[0]
-        else:
-            break
-    n=len(zws)
-    if n>1:     # Berechnet den zeitlichen Abstand zwischen dem ersten und letztem Messwert und berechnet die Umdrehung pro Minute
-	dauer=zws[n-1]-zws[0]
-	rpm=int( (60/dt.timedelta.total_seconds(dauer))*(n/anzahlsensoren))
-	       # rpm=int(dt.timedelta.total_seconds(dauer)*n)
-    elif n==0:
-         rpm=0
-    else:       # notwenig, da dauer für den ersten Wert 0 ist und man nicht durch 0 teilen darf
-	 rpm=int(len(zws)*30)
-    print('Die akutelle Umdrehungszahl pro Minute betärgt:')
-    return rpm
-    
-   
+            speichern(turbinerpm, windrpm)
 
-def signalerkennung(channel):
-    global zws
-    t0=dt.datetime.now()
-    zws.append(t0) #fuegt den akutellen Timestamp dem Zwischenspeicher für die Timestamps hinzu
+
+def signalerkennung1(channel): # Turbine
+    global zws1
+    t10=dt.datetime.now()
+    zws1.append(t10) #fuegt den akutellen Timestamp dem Zwischenspeicher für die Timestamps hinzu
+    
+def signalerkennung2(channel): # Anemometer (wind)
+    global zws2
+    t20=dt.datetime.now()
+    zws2.append(t20) #fuegt den akutellen Timestamp dem Zwischenspeicher für die Timestamps hinzu        
+       
+    # Ermittelt aus den Messdaten des Sensors einen Durchschnittswert für die Dauer der Aufloesung  
+class counter:
+    def __init__(self):
+           
+    def mittelwert(zws):
+        t1=dt.datetime.now()-dt.timedelta(0,aufloesungzm)
+        while True:
+            if len(zws)==0:
+                break   # Loescht alle Werte die aelter als die dauer der Aufloesung sind
+            elif t1>=zws[0]:
+                del zws[0]
+            else:
+                break
+        n=len(zws)
+        if n>1:     # Berechnet den zeitlichen Abstand zwischen dem ersten und letztem Messwert und berechnet die Umdrehung pro Minute
+    	dauer=zws[n-1]-zws[0]
+    	rpm=int( (60/dt.timedelta.total_seconds(dauer))*(n/anzahlsensoren))
+    	       # rpm=int(dt.timedelta.total_seconds(dauer)*n)
+        elif n==0:
+             rpm=0
+        else:       # notwenig, da dauer für den ersten Wert 0 ist und man nicht durch 0 teilen darf
+    	 rpm=int(len(zws)*30)
+        print('Die akutelle Umdrehungszahl pro Minute betärgt:')
+        return rpm
+    
         
-def speichern(rpm):
+def speichern(turbinerpm, windrpm):
         curs=db.cursor()
-        curs.execute("""INSERT INTO Umdrehungen (zeitstempel, wind) VALUES (NOW(), '%s')""" %(rpm) )
+        curs.execute("""INSERT INTO Umdrehungen (zeitstempel,rpm , wind) VALUES (NOW(), '%s', '%s')""" %(turbinerpm, windrpm) )
         curs.close()
         db.commit()   
 
 
 def main():
     #time.sleep(10)
-    global rpm
-    global aufloesungzm
-    global aufloesung
-    global db
     #db=MySQLdb.connect(host='localhost', user='verlauf', passwd='Turby', db='turbine')
     #print('Datenbank erfolgreich geöffnet')
     #aufloesungzm=int(input('Bitte den gewuenschten Zeitraum zum ermitteln des gleitenden Durchschnitts in Sekunden eingeben: '))
     #aufloesung=int(input('Bitte die gewuenschte Aufloesung in Sekunden eingeben: '))
     t1=timer()
-    GPIO.add_event_detect(27, GPIO.BOTH, callback=signalerkennung)
+    GPIO.add_event_detect(27, GPIO.BOTH, callback=signalerkennung, bouncetime=100)
+    GPIO.add_event_detect(17, GPIO.BOTH, callback=signalerkennung, bouncetime=100)
     try:
         # Loop until users quits with CTRL-C
         while True:
