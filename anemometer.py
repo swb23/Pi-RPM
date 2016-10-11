f3ac6fee9bec0da909e40fb54ff9282db29a2748
Sander Berning <sander.berning@gmail.com>
2016-10-11 17:22:26 +0200

    verbesserung

--- anemometer.py
+++ anemometer.py
@@ -72,13 +72,13 @@ class counter:
                 break
         n=len(zws)
         if n>1:     # Berechnet den zeitlichen Abstand zwischen dem ersten und letztem Messwert und berechnet die Umdrehung pro Minute
-    	dauer=zws[n-1]-zws[0]
-    	rpm=int( (60/dt.timedelta.total_seconds(dauer))*(n/anzahlsensoren))
+        dauer=zws[n-1]-zws[0]
+        rpm=int( (60/dt.timedelta.total_seconds(dauer))*(n/anzahlsensoren))
     	       # rpm=int(dt.timedelta.total_seconds(dauer)*n)
         elif n==0:
              rpm=0
         else:       # notwenig, da dauer für den ersten Wert 0 ist und man nicht durch 0 teilen darf
-    	 rpm=int(len(zws)*30)
+            rpm=int(len(zws)*30)
         print('Die akutelle Umdrehungszahl pro Minute betärgt:')
         return rpm
     
